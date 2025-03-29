6cb89fb9b9dc702b2b810e23aa75683676f971e7
Illagers <hugiheysr98@gmail.com>
2025-03-29 14:28:43 +0100

    Regard qui bouge avec la souris (Lancez pas le jeu ou votre souris va être bloquée mdr)

--- idk/script/MovePlayer.gd
+++ idk/script/MovePlayer.gd
@@ -3,6 +3,8 @@ extends Node3D
 @export var horizontalAxe : float
 @export var verticalAxe : float
 @export var direction : Vector3
+@export var camera: Node3D
+@export var Head: Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,3 +21,14 @@ func _physics_process(delta):
 	direction = Vector3(horizontalAxe, 0, verticalAxe).normalized()
 	position += direction * speed
 	pass
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			Head.rotate_y(-event.relative.x * 0.01)
+			camera.rotate_x(-event.relative.y * 0.01)
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
