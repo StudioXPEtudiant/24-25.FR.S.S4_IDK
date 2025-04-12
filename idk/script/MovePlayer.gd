039667ad40a196415675eee39e33f3e211003770
Illagers <hugiheysr98@gmail.com>
2025-04-12 15:12:31 +0200

    changement du déplacement (La caméra ne bouge plus mais j'y travaille) ajout de la possibilité d'interrargir avec des objets

--- idk/script/MovePlayer.gd
+++ idk/script/MovePlayer.gd
@@ -1,40 +1,49 @@
-extends Node3D
-@export var speed:float
-@export var horizontalAxe : float
-@export var verticalAxe : float
-@export var direction : Vector3
-@export var camera: Node3D
-@export var Head: Node3D
-var emit: bool
+extends CharacterBody3D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+# How fast the player moves in meters per second.
+@export var speed = 14
+# The downward acceleration when in the air, in meters per second squared.
+@export var fall_acceleration = 75
+
+var target_velocity = Vector3.ZERO
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+
 
 func _physics_process(delta):
-	horizontalAxe = Input.get_axis("Player_Left", "Player_Right")
-	verticalAxe = Input.get_axis("Player_Up", "Player_Down")
-	direction = Vector3(horizontalAxe, 0, verticalAxe).normalized()
-	position += direction * speed
-	pass
-	
-	
-func _input(event):
-	if event is InputEventMouseMotion:
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	elif event.is_action_pressed("ui_cancel"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		if event is InputEventMouseMotion:
-			Head.rotate_y(-event.relative.x * 0.01)
-			camera.rotate_x(-event.relative.y * 0.01)
-			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
+	var direction = Vector3.ZERO
 
-func _emit():
-	emit == true
-	pass
+	if Input.is_action_pressed("Player_Right"):
+		direction.x += 1
+	if Input.is_action_pressed("Player_Left"):
+		direction.x -= 1
+	if Input.is_action_pressed("Player_Down"):
+		direction.z += 1
+	if Input.is_action_pressed("Player_Up"):
+		direction.z -= 1
+
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		#$Pivot.basis = Basis.looking_at(direction)
+
+	# Ground Velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
+	
+	# Iterate through all collisions that occurred this frame
+	for index in range(get_slide_collision_count()):
+		# We get one of the collisions with the player
+		var collision = get_slide_collision(index)
+
+		#if there are many collision in one frame
+		if collision.get_collider() == null:
+			continue
+
+		# If the collider is with a object
+		if collision.get_collider().is_in_group("object"):
+			var mob = collision.get_collider()
+			
