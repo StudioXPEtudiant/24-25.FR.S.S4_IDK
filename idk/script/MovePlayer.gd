5be0e7cec5aec0de0ba2b295bcf87ddf76aac54a
Illagers <hugiheysr98@gmail.com>
2025-05-10 14:11:26 +0200

    o

--- idk/script/MovePlayer.gd
+++ idk/script/MovePlayer.gd
@@ -70,11 +70,7 @@ func _input(event):
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	elif event.is_action_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		if event is InputEventMouseMotion:
-			Head.rotate_y(-event.relative.x * 0.01)
-			camera.rotate_x(-event.relative.y * 0.01)
-			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
+	
 	if event.is_action_pressed("s'accroupir") and saccroupir == true:
 		s_accroupir()
 	if event.is_action_pressed("s'accroupir") and est_accroupi == false and saccroupir == false:
