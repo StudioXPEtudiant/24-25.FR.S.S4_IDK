19a97ba4f1910e3e08c5636aec63395bffa8406b
Illagers <hugiheysr98@gmail.com>
2025-05-17 14:57:36 +0200

    debut du design de la réponse

--- idk/reponse_level_1.gd
+++ idk/reponse_level_1.gd
@@ -1,20 +1,18 @@
-extends Node3D
-signal collected
-@export var joueur:CharacterBody3D 
+extends StaticBody3D
+var move : Vector3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	move = Vector3(0,1,0)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	for i in range (3):
+		global_position += move
+		i = i+1
+	for i in range (3):
+		global_position -= move
+		i = i+1
 	pass
-
-	
-
-
-func _on_body_entered(body):
-	collected.emit()
-	joueur.queue_free()
-	queue_free()
