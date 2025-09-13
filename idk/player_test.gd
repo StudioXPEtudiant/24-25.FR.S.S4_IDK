e0c245b1fe4fbf043838ef80824c86ac0633a53f
Illagers <hugiheysr98@gmail.com>
2025-09-13 14:15:28 +0200

    la camera fonctionne et creation d'un player cobaye

--- idk/player_test.gd
+++ idk/player_test.gd
@@ -18,8 +18,7 @@ signal collected
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	shapecast_s_accroupir.add_exception(self)
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -77,43 +76,6 @@ func _input(event):
 			#Head.rotate_y(-event.relative.x * 0.01)
 			#camera.rotate_x(-event.relative.y * 0.01)
 			#camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
-	
-	if event.is_action_pressed("s'accroupir") and saccroupir == true:
-		s_accroupir()
-	if event.is_action_pressed("s'accroupir") and est_accroupi == false and saccroupir == false:
-		animation.play("s_accroupir", -1, s_accroupir_vitesse)
-	if event.is_action_released("s'accroupir") and saccroupir == false:
-		#if shapecast_s_accroupir.is_colliding() == false:
-			en_train_de_s_accroupir(false)
-		#elif shapecast_s_accroupir.is_colliding() == true:
-			#verification_collision_accroupi()
-
-	
-func s_accroupir():
-	if est_accroupi == false:
-		en_train_de_s_accroupir(true)
-	elif est_accroupi == true and shapecast_s_accroupir.is_colliding() == false:
-		en_train_de_s_accroupir(false)
-
-func en_train_de_s_accroupir(state : bool):
-	match state:
-		true:
-			animation.play("s_accroupir", 0, s_accroupir_vitesse)
-		false:
-			animation.play("s_accroupir", 0, -s_accroupir_vitesse, true)
-	
-func _on_animation_player_animation_started(anim_name):
-	if anim_name == "s_accroupir":
-		est_accroupi = !est_accroupi
-
-func verification_collision_accroupi():
-	if shapecast_s_accroupir.is_colliding() == false:
-		en_train_de_s_accroupir(false)
-	if shapecast_s_accroupir.is_colliding() == true:
-		await get_tree().create_timer(0.1).timeout
-		verification_collision_accroupi()
-
-
 func _on_area_3d_body_entered(body):
 	collected.emit()
 	body.queue_free()
