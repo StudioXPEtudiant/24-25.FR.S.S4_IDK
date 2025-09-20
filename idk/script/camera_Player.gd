4233ba8d42d382d7b5917e16febeb6d8a7e9fb62
Illagers <hugiheysr98@gmail.com>
2025-09-20 14:31:45 +0200

    camera vue derrière

--- idk/script/camera_Player.gd
+++ idk/script/camera_Player.gd
@@ -1,13 +1,15 @@
 extends Camera3D
 @export var Object_to_follow : Node3D
+var height : Vector3
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	height = Vector3(0,22,65)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	global_position = Object_to_follow.global_position 
+	global_position = Object_to_follow.global_position + height
 	pass
