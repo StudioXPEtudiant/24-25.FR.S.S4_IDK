8739619bc81f07d5e9defca928c9dd2ff2c8eca8
Illagers <hugiheysr98@gmail.com>
2025-09-20 14:36:35 +0200

    camera rotation

--- idk/script/player_test.gd
+++ idk/script/player_test.gd
@@ -67,11 +67,11 @@ func _input(event):
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	elif event.is_action_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	#if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		#if event is InputEventMouseMotion:
-			#Head.rotate_y(-event.relative.x * 0.01)
-			#camera.rotate_x(-event.relative.y * 0.01)
-			#camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			camera.rotate_y(-event.relative.x * 0.01)
+			camera.rotate_x(-event.relative.y * 0.01)
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
 func _on_area_3d_body_entered(body):
 	if body is RigidBody3D:
 		collected.emit()
