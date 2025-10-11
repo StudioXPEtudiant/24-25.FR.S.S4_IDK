4531ec1290aee009d875001af4cc2488dc0a1ba4
Illagers <hugiheysr98@gmail.com>
2025-10-11 14:41:04 +0200

    test 4

--- idk/script/camera_Player.gd
+++ idk/script/camera_Player.gd
@@ -5,7 +5,7 @@ var height : Vector3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	height = Vector3(0,22,65)
+	height = Vector3(0,5,20)
 	pass # Replace with function body.
 
 
