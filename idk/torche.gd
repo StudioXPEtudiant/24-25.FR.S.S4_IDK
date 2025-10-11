8cd0e40c2e9dcae961f23259c96b8d1795448153
Illagers <hugiheysr98@gmail.com>
2025-10-11 14:05:11 +0200

    creation du script torche (spoiler ca marche pas)

--- idk/torche.gd
+++ idk/torche.gd
@@ -1,8 +1,12 @@
 extends Node3D
+@export var timer: Node
+@export var Object_to_follow : Node3D
+var height : Vector3
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var height = Vector3(10,22,10)
 	pass # Replace with function body.
 
 
@@ -12,4 +16,11 @@ func _process(delta):
 
 
 func _on_area_3d_body_entered(body):
+	timer.start()
+	global_position = Object_to_follow.global_position + height
+	pass # Replace with function body.
+
+
+func _on_timer_timeout():
+	queue_free
 	pass # Replace with function body.
