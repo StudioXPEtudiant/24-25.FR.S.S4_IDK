0e86bffa885a45c03a890aaa69c192f466943fe0
Illagers <hugiheysr98@gmail.com>
2025-10-18 14:23:53 +0200

    premier test d'un ennemy (du genre un garde qui patrouille ou un soldat non agressif pour le moment) ça ne marche pas

--- idk/ennemy_test.gd
+++ idk/ennemy_test.gd
@@ -1,15 +1,15 @@
-extends Camera3D
-@export var Object_to_follow : Node3D
-var height : Vector3
-
+extends CharacterBody3D
+@export var Speed = 8
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	height = Vector3(0,3,6)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	global_position = Object_to_follow.global_position + height
 	pass
+
+func physic_process(delta):
+	velocity = Vector3.FORWARD * Speed
+	move_and_slide()
