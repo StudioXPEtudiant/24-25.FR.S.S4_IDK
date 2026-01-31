36f04945aa1f188bc9daea6cb4c3891c0cb7424c
Illagers <hugiheysr98@gmail.com>
2026-01-31 14:35:39 +0100

    ajout d'un deuxième ennemi du meme type

---
+++ idk/objet_Test/feu_follet2.gd
@@ -0,0 +1,19 @@
+extends RigidBody3D
+@export var timer : Timer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer.play("feu_follet2")
+	pass # Replace with function body.
+
+
+func _on_area_3d_body_entered(body):
+	if body is CharacterBody3D:
+		body.queue_free()
+	pass # Replace with function body.
+
+
+func _on_timer_timeout():
+	timer.start
+	$AnimationPlayer.play("feu_follet2")
+	pass # Replace with function body.
