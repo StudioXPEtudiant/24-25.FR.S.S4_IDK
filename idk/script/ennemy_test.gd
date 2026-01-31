1726d6749e81ed9ad7081021548f387067259bce
Illagers <hugiheysr98@gmail.com>
2026-01-31 14:25:31 +0100

    l'ennemi décrit une boucle de déplacement

--- idk/script/ennemy_test.gd
+++ idk/script/ennemy_test.gd
@@ -1,4 +1,5 @@
 extends RigidBody3D
+@export var timer : Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,4 +10,10 @@ func _ready():
 func _on_area_3d_body_entered(body):
 	if body is CharacterBody3D:
 		body.queue_free()
+	pass # Replace with function body.
+
+
+func _on_timer_timeout():
+	timer.start
+	$AnimationPlayer.play("feu_follet1")
 	pass # Replace with function body.
